e3549ffcd4939d721b6f12e234b012d826decb4f
yong1le <ygle.he@gmail.com>
2023-04-25 21:56:30 -0400

    add surround plugin

---
+++ plugins/surround.lua
@@ -0,0 +1,13 @@
+return {
+  "kylechui/nvim-surround",
+  keys = {
+    {"ys", desc="Add Surrounding"},
+    {"ds", desc="Delete Surrounding"},
+    {"cs", desc="Change Surrounding"},
+    {"S",  mode="v", desc="Block Surrounding"},
+  },
+  config = function()
+    require("nvim-surround").setup({
+    })
+  end,
+}
