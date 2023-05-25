def601688dd1cf5582cc4219e82aea1fe350308f
yong1le <ygle.he@gmail.com>
2023-05-24 21:21:14 -0400

    change theme

--- plugins/theme.lua
+++ plugins/theme.lua
@@ -2,15 +2,15 @@ return {
   "rebelot/kanagawa.nvim",
   config = function()
     require('kanagawa').setup({
-      compile = false,  -- enable compiling the colorscheme
+      compile = false, -- enable compiling the colorscheme
       undercurl = true, -- enable undercurls
       commentStyle = { italic = true },
       functionStyle = {},
       keywordStyle = { italic = true },
       statementStyle = { bold = true },
       typeStyle = {},
-      transparent = true,    -- do not set background color
-      dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+      transparent = false,  -- do not set background color
+      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
       terminalColors = true, -- define vim.g.terminal_color_{0,17}
       colors = {
         palette = {},
@@ -28,9 +28,9 @@ return {
       overrides = function(colors) -- add/modify highlights
         return {}
       end,
-      theme = "wave", -- Load "wave" theme when 'background' option is not set
+      theme = "wave",  -- Load "wave" theme when 'background' option is not set
       background = {
-        -- map the value of 'background' option to a theme
+                       -- map the value of 'background' option to a theme
         dark = "wave", -- try "dragon" !
         light = "lotus"
       },
