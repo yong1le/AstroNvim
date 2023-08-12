b45341594eac0981f3598a354d9a0cfed2bf3162
yong1le <ygle.he@gmail.com>
2023-08-12 10:38:00 -0400

    update neorg settings

--- plugins/notes.lua
+++ plugins/notes.lua
@@ -1,15 +1,46 @@
 return {
-  "nvim-neorg/neorg",
-  opts = {
-    load = {
-      ["core.dirman"] = {
-        config = {
-          workspaces = {
-            school = "~/Documents/notes/school",
-            bible = "~/Documents/notes/bible",
+  {
+    "nvim-neorg/neorg",
+    branch = "main",
+    opts = {
+      load = {
+        ["core.defaults"] = {},
+        -- Hiding markup
+        ["core.concealer"] = {},
+        ["core.keybinds"] = {},
+        ["core.completion"] = {
+          config = {
+            engine = "nvim-cmp",
+          },
+        },
+        ["core.journal"] = {},
+        -- Keep track of notebooks
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              school = "~/Documents/notes/school",
+              bible = "~/Documents/notes/bible",
+            },
+          },
+        },
+        -- Indenting
+        ["core.promo"] = {},
+        ["core.itero"] = {},
+        ["core.esupports.indent"] = {},
+        -- Exporting
+        ["core.tangle"] = {},
+        ["core.export"] = {},
+        -- Presenting
+        ["core.presenter"] = {
+          config = {
+            zen_mode = "zen-mode",
           },
         },
       },
     },
   },
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode"
+  }
 }
