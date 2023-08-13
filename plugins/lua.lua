48a67380ec481d67204e7622c62826cbd76ae6f2
yong1le <ygle.he@gmail.com>
2023-08-13 15:11:40 -0400

    book fn in utils file

--- plugins/lua.lua
+++ plugins/lua.lua
@@ -1,26 +1,10 @@
-local utils = require "astronvim.utils"
-
-local removeByValue = function(tab, value)
-  local index;
-
-  for i, v in pairs(tab) do
-    if v == value then
-      index = i;
-      break
-    end
-  end
-
-  if not (index == nil) then
-    table.remove(tab, index)
-  end
-  return tab;
-end
+local utils = require("user.utils");
 
 return {
   {
     "jay-babu/mason-null-ls.nvim",
     opts = function(_, opts)
-      opts.ensure_installed = removeByValue(opts.ensure_installed, "luacheck")
+      opts.ensure_installed = utils.removeByValue(opts.ensure_installed, "luacheck")
     end
   },
 }
