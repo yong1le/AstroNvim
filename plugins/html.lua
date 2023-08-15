5c8085da365f723fdfb8d4438cf9dc75cc081246
yong1le <ygle.he@gmail.com>
2023-08-14 22:25:48 -0400

    fix autoclose issue

--- plugins/html.lua
+++ plugins/html.lua
@@ -4,14 +4,21 @@ local aUtils = require "astronvim.utils"
 return {
   {
     "williamboman/mason-lspconfig.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = utils.removeByValue(opts.ensure_installed, "emmet_ls" )
-    end,
+    opts = function(_, opts) opts.ensure_installed = utils.removeByValue(opts.ensure_installed, "emmet_ls") end,
   },
   {
     "jay-babu/mason-null-ls.nvim",
     opts = function(_, opts)
       opts.ensure_installed = aUtils.list_insert_unique(opts.ensure_installed, "emmet-language-server")
     end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      autotag = {
+        enable = true,
+        enable_close_on_slash = false,
+      },
+    },
   },
 }
