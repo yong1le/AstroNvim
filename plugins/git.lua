7b044e8d411901886fd225d891478b9fb37d89ba
yong1le <ygle.he@gmail.com>
2023-08-14 22:01:57 -0400

    git improvements

---
+++ plugins/git.lua
@@ -0,0 +1,17 @@
+return {
+  {
+
+    "FabijanZulj/blame.nvim",
+
+    cmd = { "ToggleBlame" },
+    keys = {
+      { "<leader>gB", "<cmd>ToggleBlame<cr>", desc = "Git Blame" },
+    },
+  },
+  {
+    "sindrets/diffview.nvim",
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "View git diff"}
+    }
+  },
+}
