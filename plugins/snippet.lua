4943a4f3f3600244fdb6aea876f4916589fb97de
yong1le <ygle.he@gmail.com>
2023-08-14 22:25:42 -0400

    add snippets

--- plugins/snippet.lua
+++ plugins/snippet.lua
@@ -1,8 +1,10 @@
 return {
   {
     "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
     config = function(plugin, opts)
       require "plugins.configs.luasnip" (plugin, opts)                                         -- include the default astronvim config that calls the setup call
+      require("luasnip.loaders.from_vscode").lazy_load()
       require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } }   -- load snippets paths
     end,
   },
